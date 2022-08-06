2cb0633ee8f72ea9adf0c205c64992e93387d33e
GuilhermeOM <guilhermeomalta0@gmail.com>
2022-08-06 01:31:10 -0300

    Setting up FaunaDB and creating signIn callback for sign in registration.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query } from 'faunadb';
+
 import NextAuth from 'next-auth/next';
 import GithubProvider from 'next-auth/providers/github';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
     providers: [
@@ -8,4 +12,38 @@ export default NextAuth({
             clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
         }),
     ],
+    callbacks: {
+        async signIn({user}) {
+            const { email } = user;
+
+            try {
+                await fauna.query (
+                    query.If(
+                        query.Not(
+                            query.Exists(
+                                query.Match(
+                                    query.Index('user_by_email'),
+                                    query.Casefold(email as string)
+                                )
+                            )
+                        ),
+                        query.Create(
+                            query.Collection('users'), { data: { email } }
+                        ),
+                        query.Get(
+                            query.Match(
+                                query.Index('user_by_email'),
+                                query.Casefold(email as string)
+                            )
+                        )
+                    )
+                );
+            } catch {
+                return false;
+            }
+
+            return true;
+        }
+
+    }
 })
