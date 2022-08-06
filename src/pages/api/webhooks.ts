953889f4b8bf2bac8e48fdd17d5325e76120b422
GuilhermeOM <guilhermeomalta0@gmail.com>
2022-08-06 16:40:11 -0300

    Creating a subscription manager.

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { Readable } from 'stream';
 import Stripe from 'stripe';
 import { stripe } from '../../services/stripe';
+import { saveSubscription } from './_lib/manageSubscription';
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -42,6 +43,24 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const type = event.type;
 
     if (relevantEvents.has(type)) {
+      try {
+        switch (type) {
+          case 'checkout.session.completed':
+            const checkoutSession = event.data.object as Stripe.Checkout.Session;
+
+            await saveSubscription(
+              checkoutSession.subscription?.toString() as string,
+              checkoutSession.customer?.toString() as string
+            );
+
+            break;
+          default:
+            throw new Error('Unhandled event.');
+        }
+      } catch (error: any) {
+        return res.json({ error: 'Webhook handler failed.' });
+      }
+
       console.log('Event Received', event)
     }
 
