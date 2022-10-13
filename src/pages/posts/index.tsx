5ce3aee4a3e03fa4b286157022834f9112425000
Guilherme <guilhermeomalta0@gmail.com>
2022-10-13 00:09:49 -0300

    Creating post page per id and validating subscription to access

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "./styles.module.scss";
 import { createClient } from "../../services/prismic";
 import { GetStaticProps } from "next";
+import Link from "next/link";
 
 type Post = {
   slug: string;
@@ -24,11 +25,13 @@ export default function Posts({ posts }: PostProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <a key={post.slug} href="#">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
